819d7d322087a259962a876a248bd5e462b6d4a7
rakhi <rakhisingh876.com@gmail.com>
2025-10-23 23:22:08 +0530

    Initial commit with project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import Cart from './pages/Cart';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React from 'react'
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+    <div className='bg-blue-500'>App</div>
+  )
+}
 
-export default App;
+export default App
