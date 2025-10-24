d31a2bece376ea91e933eacfac84a48f4495333f
rakhi <rakhisingh876.com@gmail.com>
2025-10-24 20:39:37 +0530

    chore: setup Redux store and Provider in main index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
+import axios from './api/axiosconfig'
+import {asyncgetusers} from './store/userAction'
 
 const App = () => {
+ 
+  useEffect(()=>{
+    asyncgetusers()
+    // getproduct();     //useEffect() controls when the code should run.what 
+  },[])
   return (
     <div className='bg-blue-500'>App</div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; 
+import { Provider } from 'react-redux'; 
+import './index.css';
+import App from './App.jsx';
+import { store } from './store/store.jsx';
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
-    <App />
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
